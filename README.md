430076b7ea36ba2fbd349e0a51a7b1613d909651
наб <nabijaczleweli@nabijaczleweli.xyz>
2022-10-12 18:10:22 +0200

    ą

--- README.md
+++ README.md
@@ -12,3 +12,15 @@ A DKMS sign helper for (semi-)automatic signing built modules and notes on gener
 ### [`screen-rotator`](screen-rotator)
 Automatic screen rotation through the power of `/dev/iio:device0` and unsavoury use of xinput(1).
 Should even be relatively portable, though heed the big comment at the top.
+
+### [`ą`](ą.c)
+:)
+```
+$ ą
+ąęć
+ą       (U+0105, UTF-8 c4 85    ): LATIN SMALL LETTER A WITH OGONEK
+ę       (U+0119, UTF-8 c4 99    ): LATIN SMALL LETTER E WITH OGONEK
+ć       (U+0107, UTF-8 c4 87    ): LATIN SMALL LETTER C WITH ACUTE
+$ echo — | ą
+—       (U+2014, UTF-8 e2 80 94 ): EM DASH
+```
---
+++ ą.c
@@ -0,0 +1,41 @@
+/// Requires libicu-dev
+/// cc ą.c -O3 -o ~/bin/ą $(pkg-config --cflags --libs icu-uc) -D_GNU_SOURCE
+#include <unicode/uchar.h>
+#include <stdio.h>
+#include <wchar.h>
+#include <string.h>
+#include <errno.h>
+#include <locale.h>
+int main() {
+	setlocale(LC_ALL, "");
+	for(wint_t c; (c = fgetwc(stdin)) != WEOF; ) {
+		if(c == L'\n')
+			continue;
+
+		char name[4096];
+		UErrorCode err;
+		u_charName(c, U_UNICODE_CHAR_NAME, name, sizeof(name), &err);
+		if(!U_SUCCESS(err))
+			strcpy(name, "(no name)");
+
+		fprintf(stdout, "%lc\t(U+%04X, UTF-8 ", c, c);
+		uint32_t uc = c;
+		if(uc <= 0x7F) {
+			fprintf(stdout, "%02x", uc);
+		} else if(uc <= 0x7FF) {
+			fprintf(stdout, "%02x ", 0b11000000 | ((uc & 0b11111000000) >> 6));
+			fprintf(stdout, "%02x",  0b10000000 | ((uc & 0b00000111111) >> 0));
+		} else if(uc <= 0xFFFF) {
+			fprintf(stdout, "%02x ", 0b11100000 | ((uc & 0b1111000000000000) >> 12));
+			fprintf(stdout, "%02x ", 0b10000000 | ((uc & 0b0000111111000000) >> 6));
+			fprintf(stdout, "%02x",  0b10000000 | ((uc & 0b0000000000111111) >> 0));
+		} else if(uc <= 0x10FFFF) {
+			fprintf(stdout, "%02x ", 0b11110000 | ((uc & 0b111000000000000000000) >> 18));
+			fprintf(stdout, "%02x ", 0b10000000 | ((uc & 0b000111111000000000000) >> 12));
+			fprintf(stdout, "%02x ", 0b10000000 | ((uc & 0b000000000111111000000) >> 6));
+			fprintf(stdout, "%02x",  0b10000000 | ((uc & 0b000000000000000111111) >> 0));
+		} else
+			fputs("too big!", stdout);
+		fprintf(stdout, "\t): %s\n", name);
+	}
+}
